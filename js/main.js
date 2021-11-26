591d067e881c976890b57dffe6a21a525ced06f5
Letalandroid <carlossoncra@gmail.com>
2021-11-26 09:07:55 -0500

    Add funcionality to proyect

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let onOff = true;
+let volumen = 50;
 
 document.getElementById("on-off").addEventListener("click", () => {
 
@@ -17,3 +18,67 @@ document.getElementById("on-off").addEventListener("click", () => {
     }
 
 });
+
+document.getElementById("up").addEventListener("click", () => {
+
+    if (onOff) {
+
+        if (volumen >= 100) {
+
+            console.log("El dispositivo llegó al volumen máximo!!");
+            volumen -= 10;
+
+        }
+
+        volumen += 10;
+        console.log("Volumen: " + volumen);
+
+    } else if (!onOff) {
+
+        celOff();
+
+    }
+
+});
+
+document.getElementById("down").addEventListener("click", () => {
+
+    if (onOff) {
+
+        if (volumen <= 0) {
+
+            console.log("El dispositivo llegó al volumen mínimo!!");
+            volumen += 10;
+
+        }
+
+        volumen -= 10;
+        console.log("Volumen: " + volumen);
+
+    } else if (!onOff) {
+
+        celOff();
+
+    }
+
+});
+
+document.getElementById("pantalla").addEventListener("dblclick", () => {
+
+    if (onOff) {
+
+        console.log("Foto tomada y guardada en Galería");
+
+    } else if (!onOff) {
+
+        celOff();
+
+    }
+
+});
+
+function celOff() {
+    
+    console.log("El teléfono está apagado!!");
+
+}
